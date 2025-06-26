76162b6722a49bd3ac6b32e5c900b59d4c559927
rambhardwajj <rambhardwaj101000@gmail.com>
2025-06-26 14:34:47 +0530

    fixed: cookies in middleware

--- BE/src/middlewares/auth.middleware.ts
+++ BE/src/middlewares/auth.middleware.ts
@@ -5,10 +5,11 @@ import { env } from "../configs/env";
 import { decodedUser } from "../types";
 
 export const isLoggedIn = async (req: Request, res: Response, next: NextFunction) => {
-  const { accesstoken } = req.cookies;
-  if (!accesstoken) throw new CustomError(401, "You must be logged in to access this resource.");
+  const { accessToken } = req.cookies;
+  console.log(accessToken)
+  if (!accessToken) throw new CustomError(401, "You must be logged in to access this resource.");
   try {
-    const decoded = jwt.verify(accesstoken, env.ACCESS_TOKEN_SECRET)
+    const decoded = jwt.verify(accessToken, env.ACCESS_TOKEN_SECRET)
     req.user = decoded as decodedUser;
 
     next();
