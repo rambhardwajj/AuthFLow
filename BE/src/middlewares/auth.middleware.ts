57e8e396d499631d0ec8b698338fd43a7ba7c8f4
rambhardwajj <rambhardwaj101000@gmail.com>
2025-06-28 01:10:18 +0530

    fixed: authmiddleware

--- BE/src/middlewares/auth.middleware.ts
+++ BE/src/middlewares/auth.middleware.ts
@@ -6,14 +6,12 @@ import { decodedUser } from "../types";
 
 export const isLoggedIn = async (req: Request, res: Response, next: NextFunction) => {
   const { accessToken } = req.cookies;
-  console.log(accessToken)
   if (!accessToken) throw new CustomError(401, "You must be logged in to access this resource.");
   try {
     const decoded = jwt.verify(accessToken, env.ACCESS_TOKEN_SECRET)
     req.user = decoded as decodedUser;
 
     next();
-
   } catch (error) {
     if(error instanceof TokenExpiredError){
         throw new CustomError(401, error.name);
