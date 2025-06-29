1a728569bc0f136f2929a4942a0052f0d1f2a872
rambhardwajj <rambhardwaj101000@gmail.com>
2025-06-29 14:57:09 +0530

    rever: changes

--- BE/src/index.ts
+++ BE/src/index.ts
@@ -2,11 +2,9 @@
 import app from "./app"
 import { env } from "./configs/env"
 import { logger } from "./configs/logger"
-import dotenv from "dotenv"
 
-dotenv.config()
 
-const PORT = process.env.PORT  || env.PORT || 8008;
+const PORT =  env.PORT || 8008;
 
 
 app.listen(PORT, () => {
